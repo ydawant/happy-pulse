9d5e5c7dd91a2bcc3202c0c38f90bc3867b27ffe
Yannick Dawant <yannickdawant@Yannicks-MacBook-Pro.local>
2014-03-18 20:50:59 -0500

    updated user model

--- models/users.js
+++ models/users.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var usersSchema = new Schema({
-	username: String,
-	email: String,
-	date: {type: Date, default: Date.now }
+  name: String,
+  pulse: [{type: Number, default: 0} ],
+  created_at: {type: Date, default: Date.now },
+  updated_at: {type: Date, default: Date.now }
 })
 
 var Users = mongoose.model('Users', usersSchema);
